3a169146a6ece110d01ed3c671f79b83aa2953d3
Jason Willems <jason@willems.ca>
2016-12-18 17:35:50 -0800

    Upgrade phoenix_ecto so project works on Elixir 1.3.3. Redirect index page to /players path

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule Werewolves.PageController do
   use Werewolves.Web, :controller
 
   def index(conn, _params) do
-    players = length(Repo.all(Werewolves.Player))
-    render conn, "index.html", players: players
+    conn 
+    |> redirect(to: player_path(conn, :index))
   end
 end
